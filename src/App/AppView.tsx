034a3c86beb486f08c9c20886b030d2808b65033
elijah-systems <elijahndungu30@gmail.com>
2021-02-20 14:48:42 +0300

    test processing and set up

---
+++ src/App/AppView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainPage from '../MainPage'
+import './AppStyles.scss'
+
+const AppView: React.FC = () => {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  )
+}
+
+export default AppView
