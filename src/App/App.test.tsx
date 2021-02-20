f6c3704c95d7b2fc058ff1e9bc88724dad9d3854
elijah-systems <elijahndungu30@gmail.com>
2021-02-20 14:49:35 +0300

    Images

--- src/App/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './AppView'
-
-it('Component App should render without crashing', () => {
-  expect(shallow(<App />).exists()).toBeTruthy()
-})
