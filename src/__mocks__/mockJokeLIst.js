7a838beafae581e522e8fa7c3511075ba251fc77
elijah-systems <elijahndungu30@gmail.com>
2021-02-20 16:09:10 +0300

    Fully coded

---
+++ src/__mocks__/mockJokeLIst.js
@@ -0,0 +1,18 @@
+export const MOCK_LIST = [{
+  id: 'string',
+  url: 'string',
+  text: 'fav',
+  categories: 'string',
+  updatedAt: 'string',
+  isFavourite: true,
+  isDataFromServer: true
+},
+{
+  id: 'string1',
+  url: 'string',
+  text: 'notfav',
+  categories: 'string',
+  updatedAt: 'string',
+  isFavourite: false,
+  isDataFromServer: false
+}]
